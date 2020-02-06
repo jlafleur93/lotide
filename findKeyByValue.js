abbcdb436df7b171497cb98c9d662bf0a3a27856
joel-wowz <josephlafluer@hotmail.com>
2020-02-06 04:44:24 +0000

    init commit

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire",
+  cartoon: "The Simpsons"
+};
+
+const findKeyByValue = function(obj, val){
+  for(let i of Object.keys(obj)){
+    if(obj[i] === val){
+      return i;
+    } 
+  }
+}
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Simpsons"), "cartoon");
