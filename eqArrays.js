2df53f4b331b10835c0fb8b281fee1cb793edf71
joel-wowz <josephlafluer@hotmail.com>
2020-02-10 22:20:56 +0000

    removed assertequal and found out my array check doesnt work

--- eqArrays.js
+++ eqArrays.js
@@ -1,22 +1,16 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require('./assertEqual')
 function eqArrays(arr1,arr2){
-  
-  for(var i = 0; i < arr1.length; i++){
-    if(arr1[i] !== arr2[i]){
-    return false;
-  }
-  return true;
-  }
+ for(let val of arr1){
+   
+   if(arr1[val] === arr2[val]){
+     return val
+   }
+ }
 }
-eqArrays([1, 2, 3], [1, 2, 3]) // => true
-eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);// => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "4", "3"]), true) // => true
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+module.exports = eqArrays;
