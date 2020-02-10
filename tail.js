d14d330d4b8f192dbcb9232eab4a497536dada7d
joel-wowz <josephlafluer@hotmail.com>
2020-02-10 22:02:01 +0000

    removed assertEqual

--- tail.js
+++ tail.js
@@ -1,18 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
+const assertEqual = require(`./assertEqual`)
 const tail = function(x){
-  arr = [];
-  for(var i = 1; i < x.length; i++){
-    console.log(x[i]);
-    arr.push(x[i])
-  }
-  return arr;
+  return x.slice(1);
 }
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
@@ -22,3 +10,5 @@ assertEqual(words[0], "Lighthouse");
 assertEqual(words[1], "Labs");
 const newWords = ["same"];
 tail(newWords);
+
+module.exports = tail
